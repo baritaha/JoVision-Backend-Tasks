c6fe92765eb3afeb4388c0742d256a55193a08eb
baritaha <135865393+baritaha@users.noreply.github.com>
2025-06-26 10:24:39 +0300

    add Task#44

---
+++ JoVisionBackend/Controllers/GreeterController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JoVisionBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GreeterController :ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetGreeting([FromQuery] string name = "anonymous")
+        {
+            
+            return Ok($"Hello {name} greeting");
+
+        }
+    }
+}
